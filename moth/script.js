29bc17f98491ccb66244e2f7c5ebec1d0e84572b
Jacek Herbrych <jacek.herbrych@icloud.com>
2025-08-14 21:18:48 +0200

    moth test

---
+++ moth/script.js
@@ -0,0 +1,28 @@
+shuffle()
+
+function shuffleArray(array) {
+ for (let i = array.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [array[i], array[j]] = [array[j], array[i]];
+ }
+ return array;
+}
+
+function shuffle() {
+ var inst = [1, 2, 3, 4, 5, 6, 7, 8];
+ var speed = [1, 2, 3, 4, 4, 3, 2, 1];
+// var shift = [-31, -15.5, 1.5, 20, 60, 78.5, 95.5, 111];
+ var shift = [-49, -29, -7.5, 15.5, 64.5, 87.5, 109, 129];
+ instshuffled = shuffleArray(inst);
+ var str = "";
+ var j=0;
+ for (let i = 0; i <= 8; i++) {
+  if (i == 4) {
+   str += "<div data-parallax='layer' data-parallax-speed='speed5' data-image='inst0' style='left: 40vw;'></div>";
+  } else {
+   str += "<div class='layer-fade-in' data-parallax='layer' data-parallax-speed='speed" + speed[j] + "' data-image='inst" + inst[j] + "' style='left: " + shift[j] + "vw;'>" + "</div>";
+   j += 1;
+  }
+ }
+ document.getElementById("shuffle").outerHTML = str;
+}
